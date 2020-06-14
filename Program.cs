cb0d0ad5231884233c7dc2b8c6e3fbc0c1e0bc79
Bhavesh Vaghela <vaghela.bhavesh@hotmail.com>
2020-06-14 18:12:19 +0530

    adding files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World!");
+        }
+    }
+}
